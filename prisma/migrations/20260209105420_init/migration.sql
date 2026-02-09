34556059c1a5b42b1edd847993d10d4557eb80ce
Ibrahim kapasi <ibrahimkapasi999@gmail.com>
2026-02-09 19:50:22 +0530

    Switch_to_PostgreSQL_and_remove_SQLite

--- prisma/migrations/20260209105420_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "WishlistItem" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "url" TEXT NOT NULL,
-    "title" TEXT,
-    "image" TEXT,
-    "price" TEXT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
